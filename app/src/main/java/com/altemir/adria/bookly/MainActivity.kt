61bc1ba11c9111c5f3253e7087047bf3c8cf4e90
esdefranc <Adriaaltemir18@gmail.com>
2018-11-16 15:27:36 +0100

    First commit
    Start the project

---
+++ app/src/main/java/com/altemir/adria/bookly/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.altemir.adria.bookly
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
