a85880340ad9d755400890af2b91e5e79a44cafb
esdefranc <Adriaaltemir18@gmail.com>
2018-12-08 16:46:30 +0100

    SplashScreen created
    SplashScreen created and finished

---
+++ app/src/main/java/com/altemir/adria/bookly/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.altemir.adria.bookly
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window
+import android.view.WindowManager
+
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+import java.lang.Exception
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hidding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+            }, 2000)
+    }
+
+}
