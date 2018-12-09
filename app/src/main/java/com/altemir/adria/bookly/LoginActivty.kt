b730a691a5194ba104345c113621dce0f4439b8f
esdefranc <Adriaaltemir18@gmail.com>
2018-12-09 16:54:04 +0100

    Login and Main created
    Login and Main created
    -Main is the first page that user see

--- app/src/main/java/com/altemir/adria/bookly/LoginActivty.kt
+++ app/src/main/java/com/altemir/adria/bookly/LoginActivty.kt
@@ -1,16 +1,39 @@
 package com.altemir.adria.bookly
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_login_activty.*
 
 class LoginActivty : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_activty)
+
+        Login.setOnClickListener{
+            val email = Email.text.toString()
+            val password = Password.text.toString()
+
+            Log.d("Login", "Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener()
+                    .add
+        }
+
+        backToRegister.setOnClickListener{
+            finish()
+        }
+
 
     }
 
