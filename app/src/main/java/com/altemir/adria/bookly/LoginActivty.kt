319cd3e91fffb34614e25c5013ab0ae3bc44fe8d
esdefranc <Adriaaltemir18@gmail.com>
2018-12-09 15:46:21 +0100

    Created Login screen
    Log in screen created

---
+++ app/src/main/java/com/altemir/adria/bookly/LoginActivty.kt
@@ -0,0 +1,17 @@
+package com.altemir.adria.bookly
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_login_activty.*
+
+class LoginActivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_activty)
+
+    }
+
+}
